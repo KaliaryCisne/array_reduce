0a24ac15e8ee5aaa7e32f1881f1488d4be18a2a6
kaliary Cisne <35137857+KaliaryCisne@users.noreply.github.com>
2021-12-01 10:35:40 -0300

    Update array_reduce.php

--- array_reduce.php
+++ array_reduce.php
@@ -23,23 +23,23 @@ $movimentacoes_financeiras = [
     ]
 ];
 
-$movimentacao_total = array_reduce($movimentacoes_financeiras, function ($total_movimentado = 0, $movimentacao_atual) {
+$movimentacao_total = array_reduce($movimentacoes_financeiras, function ($total_movimentado, $movimentacao_atual) {
         return $total_movimentado + $movimentacao_atual['valor'];
-});
+}, 0);
 
-$cashin_total = array_reduce($movimentacoes_financeiras, function ($cashin_total = 0, $movimentacao_atual) {
+$cashin_total = array_reduce($movimentacoes_financeiras, function ($cashin_total, $movimentacao_atual) {
     if ($movimentacao_atual['tipo'] == 'cashin') {
         return $cashin_total + $movimentacao_atual['valor'];
     }
     return $cashin_total;
-});
+}, 0);
 
-$cashout_total = array_reduce($movimentacoes_financeiras, function ($cashout_total = 0, $movimentacao_atual) {
+$cashout_total = array_reduce($movimentacoes_financeiras, function ($cashout_total, $movimentacao_atual) {
     if ($movimentacao_atual['tipo'] == 'cashout') {
         return $cashout_total + $movimentacao_atual['valor'];
     }
     return $cashout_total;
-});
+}, 0);
 
 echo "total movimentado: " . $movimentacao_total . PHP_EOL;
 echo "total cashin: " . $cashin_total . PHP_EOL;
